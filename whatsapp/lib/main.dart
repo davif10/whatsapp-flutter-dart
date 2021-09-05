76c253a38208876b285c43b70db7df8143005df3
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-09-05 14:25:29 -0300

    Criando interface de login e cadastro.

--- whatsapp/lib/main.dart
+++ whatsapp/lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp/Home.dart';
+import 'package:whatsapp/Login.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
-  Firestore.instance.collection("usuarios")
-  .document("001")
-  .setData({"nome":"Davi"});
   runApp(MaterialApp(
-    home: Home(),
+    home: Login(),
+    theme: ThemeData(
+      primaryColor: Color(0xff075E54),
+      accentColor: Color(0xff25D366)
+    ),
     debugShowCheckedModeBanner: false,
   ));
 }
